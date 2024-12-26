32b246d1b06d5a12401f287734c56cf73d05f9bd
R00tx-0xf0rd <rytmix77@gmail.com>
2024-12-26 10:49:21 +0300

    analytics stage2

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -21,11 +21,43 @@ const Analytics = () => {
   };
 
   const analyze = () => {
-    const sumObj = { norm: 0,  human_hours:0};
+    const sumObj = {
+      norm: 0,
+      human_hours: 0,
+      pass_follow: 0,
+      rez_follow: 0,
+      wait_follow: 0,
+      less7:0,
+      vacations:0,
+      diseases:0,
+      distractions:0,
+      outside_depots:0,
+      business_trips:0,
+      doublers:0,
+      training_vacations:0
+    };
     periods.forEach((element) => {
+      const {
+        norm,
+        human_hours,
+        pass_follow,
+        rez_follow,
+        wait_follow,
+        less7,
+        vacations,
+        diseases,
+        distractions,
+        outside_depots,
+        business_trips,
+        doublers,
+        training_vacations,
+      } = element;
       console.log(element);
-      sumObj.norm += element.norm;
-      sumObj.human_hours += element.human_hours*element.norm;
+      sumObj.norm += norm;
+      sumObj.human_hours += human_hours * norm;
+      sumObj.pass_follow += pass_follow;
+      sumObj.rez_follow += rez_follow;
+      sumObj.wait_follow += wait_follow;
     });
     return sumObj;
   };
